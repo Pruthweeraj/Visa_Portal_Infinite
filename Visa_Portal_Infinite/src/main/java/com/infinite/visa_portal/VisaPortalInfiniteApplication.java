c5b675f73c82542c9d98872129231650d336dbed
pr <infinite>
2019-03-06 12:11:58 +0530

    ok

---
+++ Visa_Portal_Infinite/src/main/java/com/infinite/visa_portal/VisaPortalInfiniteApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.visa_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaPortalInfiniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaPortalInfiniteApplication.class, args);
+	}
+
+}
