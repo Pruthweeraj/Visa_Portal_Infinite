bb8d8760977fa0d16ee75ad904fed24f7cc363e5
pr <infinite>
2019-03-07 19:04:09 +0530

    NullPointerException

--- Visa_Portal_Infinite/src/main/java/com/infinite/visa_portal/repository/ProjectRepository.java
+++ Visa_Portal_Infinite/src/main/java/com/infinite/visa_portal/repository/ProjectRepository.java
@@ -1,10 +1,11 @@
 package com.infinite.visa_portal.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.infinite.visa_portal.entity.Project;
 
-
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 
 }
